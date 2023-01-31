8ca91ab488bbedbcf12a5861fb1a1799f0ff4ba1
youngthugfan7 <123851561+youngthugfan7@users.noreply.github.com>
2023-01-31 10:40:17 -0500

    Starting classes
    - done on school chromebook so couldnt test

---
+++ GUI.java
@@ -0,0 +1,11 @@
+import java.swing.*;
+
+public class GUI extends Main {
+    GUI(){
+
+        JFrame frame = new JFrame;
+        frame.setSize(300,400);
+        frame.setVisible(true);
+        frame.setResizeable(false);
+    }
+}

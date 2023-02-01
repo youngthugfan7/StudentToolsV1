5d80e32b46699e4963487470e40ed1ec8527fd9d
youngthugfan7 <alifesaverme@gmail.com>
2023-01-31 21:08:48 -0500

    debugged +  issue w not all keys showing on keypad

--- GUI.java
+++ GUI.java
@@ -1,32 +1,63 @@
-import java.swing.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.GridLayout;
+import java.awt.Color;
 
 public class GUI extends Main implements ActionListener{
     JPanel keypad;
-    JPanel button1;
-    JPanel button2;
-    JPanel button3;
-    JPanel button4;
-    JPanel button5;
-    JPanel button6;
-    JPanel button7;
-    JPanel button8;
-    JPanel button9;
-    GUI(){
-        // keyboard pannel 1-9 keys -> zero key will be  on a diffrent file 
+    JButton button0;
+    JButton button1;
+    JButton button2;
+    JButton button3;
+    JButton button4;
+    JButton button5;
+    JButton button6;
+    JButton button7;
+    JButton button8;
+    JButton button9;
+
+
+    GUI(){ // keyboard pannel 0-9 keys -> zero key will be  on a diffrent file 
         keypad = new JPanel();
-        button1 = new JPanel();
-        button2 = new JPanel();
-        button3 = new JPanel();
-        button4 = new JPanel();
-        button5 = new JPanel();
-        button6 = new JPanel();
-        button8 = new JPanel();
-        button9 = new JPanel();
-        keypad.setLayout(new GridLayout(3,3,5,0));
-        
-        JFrame frame = new JFrame;
+        button0 = new JButton("1");
+        button1 = new JButton("2");
+        button2 = new JButton("3");
+        button3 = new JButton("4");
+        button4 = new JButton("5");
+        button5 = new JButton("6");
+        button7 = new JButton("7");
+        button8 = new JButton("8");
+        button9 = new JButton("9");
+        keypad.setBounds(50, 300, 225, 200);
+        keypad.setBackground(Color.RED);
+        keypad.setLayout(new GridLayout(3,3,0,0));
+
+
+        JFrame frame = new JFrame();
         frame.setSize(400,600);
         frame.setVisible(true);
-        frame.setResizeable(false);
+        frame.setResizable(false);
+        frame.setLayout(null);
+
+        frame.add(keypad);
+        
+        keypad.add(button0);
+        keypad.add(button1);
+        keypad.add(button2);
+        keypad.add(button3);
+        keypad.add(button4);
+        keypad.add(button5);
+        keypad.add(button6);
+        keypad.add(button7);
+        keypad.add(button8);
+        keypad.add(button9);
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
     }
 }
